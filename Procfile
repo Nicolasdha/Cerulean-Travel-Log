5908cb0077ee9ece4d356cec8eae6cb1d4c398e8
Nicolasdha <sankp001@gmail.com>
2020-09-08 09:39:39 -0600

    Changed formatting for drop down menu opaque background

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn learning_log.wsgi --log-file -
+web: gunicorn Travel_Log.wsgi --log-file -
--- users/views.py
+++ users/views.py
@@ -20,6 +20,6 @@ def register(request):
 			authenticated_user = authenticate(username=new_user.username,
 					password=request.POST ['password1'])
 			login(request, authenticated_user)
-			return HttpResponseRedirect(reverse('learning_logs:index'))
+			return HttpResponseRedirect(reverse('Travel_Logs:index'))
 	context = {'form':form}
 	return render(request, 'users/register.html', context)
